851dca40d4067b6aff4d2912f0bbca933178cf2e
inogai <inogai.sign@gmail.com>
2025-01-25 19:31:29 +0800

    plugins: colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'inogai/moegi.nvim',
+    lazy = false,
+    dependencies = {
+      'rktjmp/lush.nvim',
+    },
+    priority = 1001,
+    config = function()
+      vim.cmd([[colorscheme moegi]])
+    end,
+  },
+  {
+    'ellisonleao/gruvbox.nvim',
+  },
+}
