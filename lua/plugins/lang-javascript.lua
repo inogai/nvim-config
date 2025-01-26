06e4535a4b4404ddf39b0f5e3b4ebb02ab24bc2c
inogai <inogai.sign@gmail.com>
2025-01-26 18:52:56 +0800

    lang: copy vtsls opts from lazyvim

--- lua/plugins/lang-javascript.lua
+++ lua/plugins/lang-javascript.lua
@@ -13,7 +13,29 @@ return {
       servers = {
         vtsls = {
           settings = {
+            vtsls = {
+              enableMoveToFileCodeAction = true,
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                maxInlayHintLength = 30,
+                completion = {
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+            },
             typescript = {
+              updateImportsOnFileMove = { enabled = 'always' },
+              suggest = {
+                completeFunctionCalls = true,
+              },
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = 'literals' },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = false },
+              },
               preferences = {
                 importModuleSpecifier = 'non-relative',
                 importModuleSpecifierEnding = 'js',
