fcc74fcb33696e021ab33148678626a2b4f34e65
inogai <inogai.sign@gmail.com>
2025-01-26 18:20:39 +0800

    plugins: chezmoi from LazyVim

---
+++ lua/plugins/utils-chezmoi.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    'xvzc/chezmoi.nvim',
+    lazy = true,
+    opts = {
+      edit = {
+        watch = false,
+        force = false,
+      },
+      notification = {
+        on_open = true,
+        on_apply = true,
+        on_watch = false,
+      },
+      telescope = {
+        select = { '<CR>' },
+      },
+    },
+    init = function()
+      -- run chezmoi edit on file enter
+      vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+        pattern = { os.getenv('HOME') .. '/.local/share/chezmoi/*' },
+        callback = function() vim.schedule(require('chezmoi.commands.__edit').watch) end,
+      })
+    end,
+  },
+  {
+    'echasnovski/mini.icons',
+    opts = {
+      file = {
+        ['.chezmoiignore'] = { glyph = '', hl = 'MiniIconsGrey' },
+        ['.chezmoiremove'] = { glyph = '', hl = 'MiniIconsGrey' },
+        ['.chezmoiroot'] = { glyph = '', hl = 'MiniIconsGrey' },
+        ['.chezmoiversion'] = { glyph = '', hl = 'MiniIconsGrey' },
+        ['bash.tmpl'] = { glyph = '', hl = 'MiniIconsGrey' },
+        ['json.tmpl'] = { glyph = '', hl = 'MiniIconsGrey' },
+        ['ps1.tmpl'] = { glyph = '󰨊', hl = 'MiniIconsGrey' },
+        ['sh.tmpl'] = { glyph = '', hl = 'MiniIconsGrey' },
+        ['toml.tmpl'] = { glyph = '', hl = 'MiniIconsGrey' },
+        ['yaml.tmpl'] = { glyph = '', hl = 'MiniIconsGrey' },
+        ['zsh.tmpl'] = { glyph = '', hl = 'MiniIconsGrey' },
+      },
+    },
+  },
+}
