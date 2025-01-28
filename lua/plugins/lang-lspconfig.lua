268214a06dc69dab3a6b0dd22f8c21f265d31b1f
inogai <inogai.sign@gmail.com>
2025-01-28 20:16:17 +0800

    keys: uh for inlay hints

--- lua/plugins/lang-lspconfig.lua
+++ lua/plugins/lang-lspconfig.lua
@@ -52,7 +52,7 @@ return {
           end
 
           if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-            map('<leader>th', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf })) end, '[T]oggle Inlay [H]ints')
+            map('<leader>uh', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf })) end, 'Inlay [H]ints')
           end
         end,
       })
