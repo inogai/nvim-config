94105cc99d394d645dc10befd5c0f3c3355e611d
inogai <inogai.sign@gmail.com>
2025-03-23 13:54:09 +0800

    feat(lang-js): commentstring for jsx and more

--- lua/plugins/lang-javascript.lua
+++ lua/plugins/lang-javascript.lua
@@ -8,6 +8,23 @@ return {
   },
 
   {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    ft = { 'html', 'vue', 'javascriptreact', 'typescriptreact' },
+    opts = {
+      enable_autocmd = false,
+    },
+    config = function(opts)
+      require('ts_context_commentstring').setup(opts)
+
+      local get_option = vim.filetype.get_option
+
+      vim.filetype.get_option = function(filetype, option)
+        return option == 'commentstring' and require('ts_context_commentstring.internal').calculate_commentstring() or get_option(filetype, option)
+      end
+    end,
+  },
+
+  {
     'neovim/nvim-lspconfig',
     opts = {
       servers = {
