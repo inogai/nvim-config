e0fb57ea1d015cceae1f3ab6737b0928a0efa75f
inogai <inogai.sign@gmail.com>
2025-03-26 22:35:39 +0800

    feat(ai): avante config update

--- lua/plugins/coding-ai.lua
+++ lua/plugins/coding-ai.lua
@@ -13,24 +13,40 @@ return {
   },
   {
     'yetone/avante.nvim',
-    event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
-    version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+    event = 'VeryLazy',
+    version = false, -- Never set this value to "*"! Never!
     ---@type avante.Config
     opts = {
       -- add any opts here
       provider = 'copilot',
+      auto_suggestions_provider = 'copilot',
+
+      copilot = {
+        -- model = 'claude-3.7-sonnet',
+        model = 'gpt-4o',
+      },
+
+      behaviour = {
+        -- enable_claude_text_editor_tool_mode = true,
+      },
+
+      file_selector = 'fzf',
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     build = vim.fn.has('unix') == 1 and 'make' or 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false',
     dependencies = {
+      'nvim-treesitter/nvim-treesitter',
       'stevearc/dressing.nvim',
       'nvim-lua/plenary.nvim',
       'MunifTanjim/nui.nvim',
       --- The below dependencies are optional,
       -- 'echasnovski/mini.pick', -- for file_selector provider mini.pick
       -- 'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
-      'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+      -- 'echasnovski/mini.pick', -- for file_selector provider mini.pick
+      -- 'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
+      -- 'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
       'ibhagwan/fzf-lua', -- for file_selector provider fzf
+      -- 'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
       'zbirenbaum/copilot.lua', -- for providers='copilot'
       -- {
       --   -- support for image pasting
@@ -49,14 +65,14 @@ return {
       --     },
       --   },
       -- },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { 'markdown', 'Avante' },
-        },
-        ft = { 'markdown', 'Avante' },
-      },
+      -- {
+      --   -- Make sure to set this up properly if you have lazy=true
+      --   'MeanderingProgrammer/render-markdown.nvim',
+      --   opts = {
+      --     file_types = { 'markdown', 'Avante' },
+      --   },
+      --   ft = { 'markdown', 'Avante' },
+      -- },
     },
   },
 }
