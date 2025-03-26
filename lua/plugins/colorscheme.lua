a85373bc9bb597d87b3aee6426ec35a65a9860c6
inogai <inogai.sign@gmail.com>
2025-03-26 23:15:50 +0800

    feat: colorscheme catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,13 +5,22 @@ return {
     dependencies = {
       'rktjmp/lush.nvim',
     },
-    priority = 1001,
-    config = function() vim.cmd([[colorscheme moegi]]) end,
+    -- priority = 1001,
+    -- config = function() vim.cmd([[colorscheme moegi]]) end,
   },
   {
     'folke/tokyonight.nvim',
   },
   {
     'ellisonleao/gruvbox.nvim',
+  },
+  {
+    'rebelot/kanagawa.nvim',
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1001,
+    config = function() vim.cmd([[colorscheme catppuccin]]) end,
   },
 }
