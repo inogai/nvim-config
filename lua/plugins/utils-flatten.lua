e635baee56704b7c3ed7b2d3559684dc972bd3ac
inogai <inogai.sign@gmail.com>
2025-04-17 21:32:14 +0800

    feat(utils): flatten

---
+++ lua/plugins/utils-flatten.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'willothy/flatten.nvim',
+    opts = {},
+    -- Ensure that it runs first to minimize delay when opening file from terminal
+    lazy = false,
+    priority = 1002,
+  },
+}
