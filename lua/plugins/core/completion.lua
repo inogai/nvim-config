7ecc2ea658b1caaceb59fd644d2c340f013db77c
inogai <inogai.sign@gmail.com>
2025-05-12 18:22:54 +0800

    ref(completions): extract out providers

--- lua/plugins/core/completion.lua
+++ lua/plugins/core/completion.lua
@@ -42,6 +42,49 @@ local function node_under_cursor(node)
   return false
 end
 
+---@type table<string, blink.cmp.SourceProviderConfigPartial>
+local providers = {
+  avante = {
+    name = 'avante',
+    module = 'blink-cmp-avante',
+    score_offset = 1000,
+    async = true,
+
+    opts = {
+      avante = {
+        command = {
+          get_kind_name = function(_) return 'AvanteCmd' end,
+        },
+        mention = {
+          get_kind_name = function(_) return 'AvanteMention' end,
+        },
+      },
+
+      kind_icons = {
+        AvanteCmd = '',
+        AvanteMention = '',
+      },
+    },
+  },
+
+  copilot = {
+    name = 'copilot',
+    module = 'blink-cmp-copilot',
+    score_offset = 100,
+    async = true,
+
+    transform_items = function(_, items)
+      local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
+      local kind_idx = #CompletionItemKind + 1
+      CompletionItemKind[kind_idx] = 'Copilot'
+      for _, item in ipairs(items) do
+        item.kind = kind_idx
+      end
+      return items
+    end,
+  },
+}
+
 return {
   {
     'echasnovski/mini.snippets',
@@ -126,48 +169,7 @@ return {
 
       sources = {
         default = { 'copilot', 'avante', 'lsp', 'path', 'snippets', 'buffer' },
-
-        providers = {
-          avante = {
-            name = 'avante',
-            module = 'blink-cmp-avante',
-            score_offset = 1000,
-            async = true,
-
-            opts = {
-              avante = {
-                command = {
-                  get_kind_name = function(_) return 'AvanteCmd' end,
-                },
-                mention = {
-                  get_kind_name = function(_) return 'AvanteMention' end,
-                },
-              },
-
-              kind_icons = {
-                AvanteCmd = '',
-                AvanteMention = '',
-              },
-            },
-          },
-
-          copilot = {
-            name = 'copilot',
-            module = 'blink-cmp-copilot',
-            score_offset = 100,
-            async = true,
-
-            transform_items = function(_, items)
-              local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
-              local kind_idx = #CompletionItemKind + 1
-              CompletionItemKind[kind_idx] = 'Copilot'
-              for _, item in ipairs(items) do
-                item.kind = kind_idx
-              end
-              return items
-            end,
-          },
-        },
+        providers = providers,
       },
     },
     opts_extend = { 'sources.default' },
