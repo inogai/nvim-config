a0680c5eda4c967d17667e7fadad377909f7e613
inogai <inogai.sign@gmail.com>
2025-06-05 00:59:23 +0800

    ref(lspconfig): split mason out

--- lua/plugins/core/lspconfig.lua
+++ lua/plugins/core/lspconfig.lua
@@ -1,12 +1,20 @@
 return {
   {
+    'williamboman/mason.nvim',
+    lazy = false,
+    priority = 1001,
+    opts = {},
+  },
+
+  {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
+    lazy = false,
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'williamboman/mason.nvim', opts = {} },
+      'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
     },
