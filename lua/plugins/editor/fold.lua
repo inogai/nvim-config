18e033de2201926216a639c6cd3f46bbc00ec9ce
inogai <inogai.sign@gmail.com>
2025-07-15 22:18:19 +0800

    feat(editor): configure ufold

--- lua/plugins/editor/fold.lua
+++ lua/plugins/editor/fold.lua
@@ -1,13 +1,28 @@
+vim.o.foldcolumn = '0' -- hide fold column
+vim.o.foldlevel = 99 -- Using ufo provider need a large value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
 return {
   {
     'kevinhwang91/nvim-ufo',
+    lazy = false,
     dependencies = { 'kevinhwang91/promise-async' },
-    init = function()
-      vim.o.foldcolumn = '1' -- '0' is not bad
-      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-    end,
     opts = {},
+    keys = {
+      { 'zR', function() require('ufo').openAllFolds() end, desc = 'Open all folds' },
+      { 'zM', function() require('ufo').closeAllFolds() end, desc = 'Close all folds' },
+
+      {
+        'K',
+        function()
+          local win_id = require('ufo').peekFoldedLinesUnderCursor()
+          if not win_id then
+            vim.lsp.buf.hover()
+          end
+        end,
+        desc = 'Hover',
+      },
+    },
   },
 }
