ddcaa7f1530d18cbd53dac0441e9226c2ebc47f4
inogai <inogai.sign@gmail.com>
2025-07-29 14:49:22 +0800

    feat(snacks/images/math): update typst template

--- lua/plugins/editor/image.lua
+++ lua/plugins/editor/image.lua
@@ -1,5 +1,34 @@
 ---@module 'snacks'
 
+local typst_template = [[
+#import "@preview/whalogen:0.3.0": ce
+#import "@preview/fancy-units:0.1.1": qty, unit
+
+#set page(width: auto, height: auto, margin: (x: 1pt, y: 0pt))
+#show math.equation.where(block: false): set text(top-edge: "bounds", bottom-edge: "bounds")
+#set text(size: 12pt, fill: rgb("${color}"))
+
+#let ub(it) = { $upright(bold(it))$ }
+#let xx = $times$
+
+#let height-grid(height: 100pt, body) = {
+  let body_height = measure(body).height
+
+  if body_height <= 16pt {
+    return box(height: height, align(horizon, body))
+  }
+
+  let lines = calc.ceil(body_height / height)
+
+  box(height: height * lines, align(horizon, body))
+}
+
+#context height-grid(height: 12pt)[
+  ${header}
+  ${content}
+]
+]]
+
 return {
   {
     'inogai/snacks.nvim',
@@ -18,12 +47,7 @@ return {
         },
         math = {
           typst = {
-            tpl = [[
-            #set page(width: auto, height: auto, margin: (x: 1pt, y: 2pt))
-            #show math.equation.where(block: false): set text(top-edge: "bounds", bottom-edge: "bounds")
-            #set text(size: 12pt, fill: rgb("${color}"))
-            ${header}
-            ${content}]],
+            tpl = typst_template,
           },
         },
       },
