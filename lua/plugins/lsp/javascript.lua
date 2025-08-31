0153261aeaa09d71a0e3f8fe26c92e6c171074a2
inogai <inogai.sign@gmail.com>
2025-08-31 21:54:25 +0800

    feat(lsp/js): reimpl moveToFile

--- lua/plugins/lsp/javascript.lua
+++ lua/plugins/lsp/javascript.lua
@@ -9,7 +9,7 @@ vim.lsp.enable('eslint')
 vim.lsp.config('vtsls', {
   settings = {
     vtsls = {
-      -- enableMoveToFileCodeAction = true,
+      enableMoveToFileCodeAction = true,
       autoUseWorkspaceTsdk = true,
       experimental = {
         maxInlayHintLength = 30,
@@ -41,6 +41,56 @@ vim.lsp.config('vtsls', {
     },
   },
 })
+
+-- required for ``enableMoveToFileCodeAction = true``
+Utils.lsp_on_attach_v2('vtsls', function(client, buffer)
+  client.commands['_typescript.moveToFileRefactoring'] = function(command, ctx)
+    ---@type string, string, lsp.Range
+    local action, uri, range = unpack(command.arguments)
+
+    local function move(newf)
+      client:request('workspace/executeCommand', {
+        command = command.command,
+        arguments = { action, uri, range, newf },
+      })
+    end
+
+    local fname = vim.uri_to_fname(uri)
+    client:request('workspace/executeCommand', {
+      command = 'typescript.tsserverRequest',
+      arguments = {
+        'getMoveToRefactoringFileSuggestions',
+        {
+          file = fname,
+          startLine = range.start.line + 1,
+          startOffset = range.start.character + 1,
+          endLine = range['end'].line + 1,
+          endOffset = range['end'].character + 1,
+        },
+      },
+    }, function(_, result)
+      ---@type string[]
+      local files = result.body.files
+      table.insert(files, 1, 'Enter new path...')
+      vim.ui.select(files, {
+        prompt = 'Select move destination:',
+        format_item = function(f) return vim.fn.fnamemodify(f, ':~:.') end,
+      }, function(f)
+        if f and f:find('^Enter new path') then
+          vim.ui.input({
+            prompt = 'Enter move destination:',
+            default = vim.fn.fnamemodify(fname, ':h') .. '/',
+            completion = 'file',
+          }, function(newf)
+            if newf then move(newf) end
+          end)
+        elseif f then
+          move(f)
+        end
+      end)
+    end)
+  end
+end)
 
 return {
   Utils.ts_ensure_installed({ 'jsdoc', 'javascript', 'typescript', 'html' }),
