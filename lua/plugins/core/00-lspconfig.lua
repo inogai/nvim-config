34b4212ad4798ab6fc00127cc41356b76ba682e4
inogai <inogai.sign@gmail.com>
2025-09-20 18:19:08 +0800

    ref(core/lspconf): organize

---
+++ lua/plugins/core/00-lspconfig.lua
@@ -0,0 +1,31 @@
+---@type LazyPluginSpec[]
+return {
+  {
+    -- Manages LSP servers, DAP adapters, linters, and formatters.
+    'mason-norg/mason.nvim',
+    lazy = false,
+    priority = 1001,
+    opts = {},
+  },
+  {
+    -- Simplifies LSP server configuration with community-maintained defaults.
+    'neovim/nvim-lspconfig',
+    lazy = false,
+  },
+  {
+    -- Connects mason.nvim with nvim-lspconfig, simplifying server setup.
+    -- Provides mappings between mason names and lsp names
+    'mason-org/mason-lspconfig.nvim',
+    lazy = false,
+    opts = {
+      automatic_enable = false,
+    },
+  },
+  {
+    -- Declaratively installs tools via mason.nvim.
+    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    lazy = false,
+    opts = { ensure_installed = {} },
+    opts_extend = { 'ensure_installed' },
+  },
+}
