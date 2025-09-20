35f1e0ca2bd5e1389b7a9655b7e679d3532555c7
inogai <inogai.sign@gmail.com>
2025-09-20 18:11:49 +0800

    core(lspconf): setup mason-lspconfig properly

--- lua/plugins/core/lspconfig.lua
+++ lua/plugins/core/lspconfig.lua
@@ -8,11 +8,15 @@ return {
   },
 
   {
+    'mason-org/mason-lspconfig.nvim',
+    opts = {
+      automatic_enable = false,
+    },
+  },
+
+  {
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     lazy = false,
-    dependencies = {
-      'mason-org/mason-lspconfig.nvim', -- allow lspconfig package names,
-    },
     opts = { ensure_installed = {} },
     opts_extend = { 'ensure_installed' },
   },
