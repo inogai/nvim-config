8ad8f69f209fa81e18a57c1db7ecd124d7e88655
inogai <inogai.sign@gmail.com>
2025-10-12 12:37:45 +0800

    Revert "opts: set shell to nu"
    This reverts commit 7f7a50365402aed85ed43e30be312fe706e88ee2.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -53,9 +53,6 @@ vim.opt.timeoutlen = 300
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- Shell
-vim.opt.shell = 'nu'
-
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
