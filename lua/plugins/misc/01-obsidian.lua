75f0b19b17068139a756b2c5c57cd9ea149ecc97
inogai <inogai.sign@gmail.com>
2025-10-12 12:37:18 +0800

    feat(obsidian): frontmatter for blog

--- lua/plugins/misc/01-obsidian.lua
+++ lua/plugins/misc/01-obsidian.lua
@@ -1,5 +1,24 @@
 local path_to_my_vault = vim.fn.expand('~') .. '/Documents/Obsidian'
 
+---@param note obsidian.Note
+---@return table<string, any>
+local frontmatter_func = function(note)
+  local now = os.date('%Y-%m-%d')
+  local out = {
+    id = note.id,
+    title = note.title,
+    description = '',
+    aliases = note.aliases,
+    tags = note.tags,
+    created = now,
+    updated = '',
+  }
+
+  out = vim.tbl_deep_extend('force', out, note.metadata or {})
+  out.updated = now
+  return out
+end
+
 return {
   {
     -- This plugin provides integration between Neovim and Obsidian.md
@@ -12,6 +31,11 @@ return {
     ---@module 'obsidian'
     ---@type obsidian.config
     opts = {
+      frontmatter = {
+        enabled = true,
+        func = frontmatter_func,
+        sort = { 'id', 'title', 'description', 'aliases', 'tags', 'created', 'updated' },
+      },
       legacy_commands = false,
       ui = { enable = false }, -- in flavour of render-markdown.nvim
       workspaces = {
