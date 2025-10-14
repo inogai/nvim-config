8103726d0bb7f8e26200f95d5d65f4bf8f97bd58
inogai <inogai.sign@gmail.com>
2025-10-14 13:50:48 +0800

    keys(mini.surround): rework + delete visual

--- lua/plugins/buffer/02-mini-surround.lua
+++ lua/plugins/buffer/02-mini-surround.lua
@@ -1,20 +1,19 @@
 return {
   {
     -- Provide mappings to easily delete, change, add, and find surroundings
-    'nvim-mini/mini.surround',
+    'inogai/mini.surround',
     event = { 'BufReadPost', 'BufWritePost', 'BufNewFile' },
     opts = {
-      custom_surroundings = {
-        ['('] = { output = { left = '(', right = ')' } },
-      },
+      visualize_delete = true,
       mappings = {
-        add = 'sa', -- Add surrounding in Normal and Visual modes
-        delete = 'sd', -- Delete surrounding
-        find = 'sf', -- Find surrounding (to the right)
-        find_left = 'sF', -- Find surrounding (to the left)
-        highlight = 'sh', -- Highlight surrounding
-        replace = 'sr', -- Replace surrounding
-        update_n_lines = 'sn', -- Update `n_lines`
+        add = 'ga', -- Add surrounding in Normal and Visual modes
+        delete = 'gs', -- Delete surrounding
+        replace = 'ge', -- Replace surrounding
+
+        -- find = 'sf', -- Find surrounding (to the right)
+        -- find_left = 'sF', -- Find surrounding (to the left)
+        -- highlight = 'sh', -- Highlight surrounding
+        -- update_n_lines = 'sn', -- Update `n_lines`
 
         suffix_last = 'l', -- Suffix to search with "prev" method
         suffix_next = 'n', -- Suffix to search with "next" method
